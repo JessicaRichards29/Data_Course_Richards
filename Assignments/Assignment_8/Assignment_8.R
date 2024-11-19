3adecf3d9f51d20afa848601c24c1f9493e2546d
JessicaRichards29 <jellybean.jessica@gmail.com>
2024-11-19 12:17:02 -0700

    assignment 8

---
+++ Assignments/Assignment_8/Assignment_8.R
@@ -0,0 +1,18 @@
+#1 loads the “/Data/mushroom_growth.csv” data set
+#2 creates several plots exploring relationships between the response and predictors
+#3 defines at least 4 models that explain the dependent variable “GrowthRate”
+#4 calculates the mean sq. error of each model
+#5 selects the best model you tried
+#6 adds predictions based on new hypothetical values for the independent variables used in your model
+#7 plots these predictions alongside the real data
+
+#1
+library(tidyverse)
+dat <- read.csv('mushroom_growth.csv')
+
+#2
+
+
+
+
+
